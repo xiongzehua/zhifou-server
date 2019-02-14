e405a5d166239254bb5a3ab81e03c86cb808d065
xiong <313398439@qq.com>
2019-02-14 15:25:42 +0800

    整合druid

--- src/test/java/com/xiongzehua/zhifou/ZhifouApplicationTests.java
+++ src/test/java/com/xiongzehua/zhifou/ZhifouApplicationTests.java
@@ -2,8 +2,13 @@ package com.xiongzehua.zhifou;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.Duration;
+
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
