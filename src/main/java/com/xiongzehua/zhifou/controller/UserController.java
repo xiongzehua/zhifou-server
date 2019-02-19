40cce1382d461b1d25acc4858899fb71454d7121
xiong <313398439@qq.com>
2019-02-19 14:24:46 +0800

    整合jwt

--- src/main/java/com/xiongzehua/zhifou/controller/UserController.java
+++ src/main/java/com/xiongzehua/zhifou/controller/UserController.java
@@ -1,5 +1,29 @@
 package com.xiongzehua.zhifou.controller;
 
+import com.xiongzehua.zhifou.common.Response;
+import com.xiongzehua.zhifou.pojo.User;
+import com.xiongzehua.zhifou.service.UserService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@ResponseBody
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/signIn")
+    public Response signIn(User user) {
+        return null;
+    }
+
+    @GetMapping("/hello")
+    public Response signIn( ) {
+        return Response.success("hello");
+    }
 
 }
--- src/main/java/com/xiongzehua/zhifou/service/UserService.java
+++ src/main/java/com/xiongzehua/zhifou/service/UserService.java
@@ -1,8 +1,13 @@
 package com.xiongzehua.zhifou.service;
 
+import com.xiongzehua.zhifou.common.Response;
+import com.xiongzehua.zhifou.pojo.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    public User signIn(User user) {
+        return null;
+    }
 
 }
