45e387f319b361709ce9406dc3c8eca1a22bd515
GuoMiaoMiao2017 <1529548826@qq.com>
2019-03-22 11:57:59 +0800

    点赞及热度排序测试

---
+++ src/test/java/com/xiongzehua/zhifou/service/UserStarTalkServiceTest.java
@@ -0,0 +1,23 @@
+package com.xiongzehua.zhifou.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class UserStarTalkServiceTest {
+
+    @Test
+    public void createUserStarTalkTest() {
+
+    }
+
+    @Test
+    public void listTalkByStarTest() {
+
+    }
+}
